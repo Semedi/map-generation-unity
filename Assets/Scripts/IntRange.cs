fa0f7e561cd3a9305174b1d03a27e9606bc93cc0
sergio semedi barranco <sergio.semedi@gmail.com>
2016-06-24 21:17:05 +0200

    subida inicial

---
+++ Assets/Scripts/IntRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+[Serializable]
+public class IntRange
+{
+    public int m_Min; //The minimum value in this range
+    public int m_MAx; 
+
+
+
+    public IntRange(int min, int max)
+    {
+        m_MAx = max;
+        m_Min = min;
+    }
+
+    public int Random
+    {
+        get {
+            return UnityEngine.Random.Range(m_Min, m_MAx;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+
+    public int xPos;
+    public int yPos;
+    public int roomWidth;
+    public int roomHeight;
+    public Direction enteringCorridor; 
+ 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
